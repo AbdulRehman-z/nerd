c243f3cd57d31d9ea1050658bf8f416752565836
AbdulRehman-z <yousafbhaikhan10@gmail.com>
2025-05-23 13:33:10 +0500

    Add TypeScript definition for Book type

---
+++ types.d.ts
@@ -0,0 +1,15 @@
+type Book = {
+  id: number,
+  title: string,
+  author: string,
+  genre: string,
+  rating: number,
+  total_copies: number,
+  available_copies: number,
+  description: string,
+  color: string,
+  coverUrl: string,
+  videoUrl: string,
+  summary: string,
+  isLoanedBook?: boolean,
+}
