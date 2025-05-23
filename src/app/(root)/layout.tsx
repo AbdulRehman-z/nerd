be9eddbbd1ff97ccc6d47db3f241749e4179eb68
AbdulRehman-z <yousafbhaikhan10@gmail.com>
2025-05-23 11:20:44 +0500

    Refactor root layout with improved structure and spacing

--- src/app/(root)/layout.tsx
+++ src/app/(root)/layout.tsx
@@ -3,9 +3,13 @@ import { ReactNode } from "react";
 
 export default function Layout({ children }: { children: ReactNode }) {
   return (
-    <main className="flex min-h-screen flex-1 flex-col max-w-7xl mx-auto px-10 @xs:px-10">
-      <Header />
-      {children}
+    <main className="root-container">
+      <div className="max-w-7xl mx-auto">
+        <Header />
+        <div className="mt-20 pb-20">
+          {children}
+        </div>
+      </div>
     </main>
   )
 }
