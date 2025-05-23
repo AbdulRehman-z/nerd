f95cc024da623f20084cf22d9994f3bdc3a2c623
AbdulRehman-z <yousafbhaikhan10@gmail.com>
2025-05-23 11:21:19 +0500

    Fix font naming and formatting in layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,28 +7,19 @@ import { ThemeProvider } from "@/providers/theme-provider";
 
 const ibmPlexSans = localFont({
   src: [
-    {
-      path: "../../public/fonts/IBMPlexSans-Regular.ttf", weight: "400", style: "normal"
-    },
-    {
-      path: "../../public/fonts/IBMPlexSans-Medium.ttf", weight: "500", style: "normal"
-    },
-    {
-      path: "../../public/fonts/IBMPlexSans-SemiBold.ttf", weight: "600", style: "normal"
-    },
-    {
-      path: "../../public/fonts/IBMPlexSans-Bold.ttf", weight: "700", style: "normal"
-    }
-  ]
-})
-
-const bebasNue = localFont({
-  src: [
-    { path: "../../public/fonts/BebasNeue-Regular.ttf", weight: "400", style: "normal" }
+    { path: "../../public/fonts/IBMPlexSans-Regular.ttf", weight: "400", style: "normal" },
+    { path: "../../public/fonts/IBMPlexSans-Medium.ttf", weight: "500", style: "normal" },
+    { path: "../../public/fonts/IBMPlexSans-SemiBold.ttf", weight: "600", style: "normal" },
+    { path: "../../public/fonts/IBMPlexSans-Bold.ttf", weight: "700", style: "normal" },
   ],
-  variable: "--bebas-nue"
-})
+});
 
+const bebasNeue = localFont({
+  src: [
+    { path: "../../public/fonts/BebasNeue-Regular.ttf", weight: "400", style: "normal" },
+  ],
+  variable: "--bebas-neue",
+});
 
 export const metadata: Metadata = {
   title: "Nerd",
@@ -43,7 +34,9 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <head />
-      <body>
+      <body
+        className={`${ibmPlexSans.className} ${bebasNeue.variable} antialiased`}
+      >
         <ThemeProvider
           attribute="class"
           defaultTheme="system"
