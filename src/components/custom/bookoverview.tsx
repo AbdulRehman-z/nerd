baa54d5db8aa9e306d546f4d5edbdeb41c58a7e0
AbdulRehman-z <yousafbhaikhan10@gmail.com>
2025-05-23 17:19:33 +0500

    Update BookOverview component props and button text
    Fix prop naming from coverUrl to cover and change button text to
    uppercase "BORROW NOW"

--- src/components/custom/bookoverview.tsx
+++ src/components/custom/bookoverview.tsx
@@ -3,7 +3,7 @@ import { Button } from "../ui/button";
 import { BookCopy } from "lucide-react";
 import BookCover from "./bookCover";
 
-export default function BookOverview({ author, available_copies, color, coverUrl, description, genre, id, rating, summary, title, total_copies, videoUrl, isLoanedBook }: Book) {
+export default function BookOverview({ author, available_copies, cover, description, genre, id, rating, summary, title, total_copies, color, isLoanedBook }: Book) {
   return (
     <section className="book-overview">
       <div className="flex flex-1 flex-col gap-5">
@@ -34,16 +34,16 @@ export default function BookOverview({ author, available_copies, color, coverUrl
         <p className="book-description" >{description}</p>
         <Button className="book-overview_btn">
           <Image src={"/icons/book.svg"} alt="svg" width={20} height={20} />
-          <p className="font-bebas-neue text-xl text-dark-100">Borrow</p>
+          <p className="font-bebas-neue text-xl text-dark-100">BORROW NOW</p>
         </Button>
       </div>
 
       <div className="relative flex flex-1 justify-center">
         <div className="relative">
-          <BookCover variant="wide" className="z-10" coverColor={color} coverImage={coverUrl} />
+          <BookCover variant="wide" className="z-10" coverColor={color} coverImage={cover} />
 
           <div className="absolute left-16 top-10 rotate-12 opacity-40 max-sm:hidden">
-            <BookCover variant="wide" coverColor={color} coverImage={coverUrl} />
+            <BookCover variant="wide" coverColor={color} coverImage={cover} />
           </div>
         </div>
       </div>
