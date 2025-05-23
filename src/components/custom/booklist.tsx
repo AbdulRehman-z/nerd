bad3db75c93163731133c799fcca335bb93d08e4
AbdulRehman-z <yousafbhaikhan10@gmail.com>
2025-05-23 11:21:34 +0500

    Update BookList component styling and heading

--- src/components/custom/booklist.tsx
+++ src/components/custom/booklist.tsx
@@ -1,12 +1,7 @@
 export default function BookList() {
   return (
-    <section>
-      <h1>Book List</h1>
-      <ul>
-        <li>Book 1</li>
-        <li>Book 2</li>
-        <li>Book 3</li>
-      </ul>
+    <section className="">
+      <h2 className="font-bebas-neue text-4xl text-light-100">Popular Book List</h2>
     </section>
   );
 }
