58650569be02319aa9ad20af12f1b2408f63bb24
AbdulRehman-z <yousafbhaikhan10@gmail.com>
2025-05-23 11:21:48 +0500

    Refactor BookOverview to use dynamic book data props

--- src/components/custom/bookoverview.tsx
+++ src/components/custom/bookoverview.tsx
@@ -1,40 +1,52 @@
 import Image from "next/image";
 import { Button } from "../ui/button";
+import { BookCopy } from "lucide-react";
+import BookCover from "./bookCover";
 
-export default function BookOverview() {
+export default function BookOverview({ author, available_copies, color, coverUrl, description, genre, id, rating, summary, title, total_copies, videoUrl, isLoanedBook }: Book) {
   return (
-    <section className="flex justify-between items-center mt-20">
-      {/* left side */}
-      <div className="w-2/4">
-        <h1 className="text-6xl font-bold">Origin</h1>
-        <div className="flex items-center gap-x-4 mt-4">
-          <span>Author: <strong>John Doe</strong></span>
-          <span>Category: <strong>Thriller/Suspense</strong></span>
-          <div className="flex items-center gap-x-1"><Image src={"/icons/star.svg"} width={20} height={20} alt="rating" /><span><strong>4.5</strong>/5</span></div>
-        </div>
+    <section className="book-overview">
+      <div className="flex flex-1 flex-col gap-5">
+        <h1>{title}</h1>
 
-        <div className="mt-3 flex items-center gap-x-4" >
-          <span>Total books: <strong>100</strong></span>
-          <span>Avaliable books: <strong>90</strong></span>
-        </div>
-
-        <div className="mt-6 flex flex-col items-start">
-          <p className="text-base font-normal">
-            Origin is a 2017 mystery-thriller novel by American author Dan Brown. It is the fifth installment in the Robert Langdon series, following previous bestsellers such as The Da Vinci Code and Angels & Demons.
+        <div className="book-info">
+          <p>
+            By <span className="font-semibold text-light-200">{author}</span>
           </p>
-          <Button className="uppercase gap-x-2 mt-7 py-7 text-xl font-normal" size={"lg"}>
-            <Image src={"/icons/book-2.svg"} alt="book-2" width={20} height={20} />
-            Borrow Book Request
-          </Button>
+          <p>
+            Category: <span className="font-semibold text-light-200">{genre}</span>
+          </p>
+          <div className="flex flex-row gap-1">
+            <Image src={"/icons/star.svg"} alt="star" width={22} height={22} />
+            <p>{rating}</p>
+          </div>
+        </div>
+
+        <div className="book-copies">
+          <p>
+            Total Books:<span className="font-semibold text-light-200">{total_copies}</span>
+          </p>
+          <p>
+            Avaliable Books:<span className="font-semibold text-light-200">{available_copies}</span>
+          </p>
+        </div>
+
+        <p className="book-description" >{description}</p>
+        <Button className="book-overview_btn">
+          <Image src={"/icons/book.svg"} alt="svg" width={20} height={20} />
+          <p className="font-bebas-neue text-xl text-dark-100">Borrow</p>
+        </Button>
+      </div>
+
+      <div className="relative flex flex-1 justify-center">
+        <div className="relative">
+          <BookCover variant="wide" className="z-10" coverColor={color} coverImage={coverUrl} />
+
+          <div className="absolute left-16 top-10 rotate-12 opacity-40 max-sm:hidden">
+            <BookCover variant="wide" coverColor={color} coverImage={coverUrl} />
+          </div>
         </div>
       </div>
-
-      {/* right side */}
-      <div>
-        <h1>Book Details</h1>
-        <p>This is a book details.</p>
-      </div>
-
     </section>
   );
 }
