9cc740f08d88cc140cc0fc8f9738a891b263bcf8
AbdulRehman-z <yousafbhaikhan10@gmail.com>
2025-05-23 11:22:07 +0500

    Refactor header component to improve logo link accessibility

--- src/components/custom/header.tsx
+++ src/components/custom/header.tsx
@@ -14,16 +14,14 @@ export default function Header() {
   return (
     <header className="flex justify-between items-center my-10">
 
-      <div className="flex items-center gap-x-2">
+      <Link className="flex gap-x-2" href="/">
         <Image src="/icons/logo.svg" alt="Logo" width={40} height={40} />
-        <Link className="font-bold text-3xl tracking-wide" href="/">
-          NERD.
-        </Link>
-      </div>
+        <span className="font-bold text-3xl tracking-wide ">NERD.</span>
+      </Link>
 
-      <div className="flex items-center gap-x-9">
-        <Link className={cn("text-base no-underline", pathname === "/library" ? "font-semibold" : "")} href="/library">Library</Link>
-        <Link className={cn("text-base no-underline", pathname === "/search" ? "font-semibold" : "")} href="/search">Search</Link>
+      <div className="flex items-center gap-x-8">
+        <Link className={cn("text-base no-underline", pathname === "/library" ? "text-light-200" : "text-light-100")} href="/library">Library</Link>
+        <Link className={cn("text-base no-underline", pathname === "/search" ? "text-light-200" : "text-light-100")} href="/search">Search</Link>
       </div>
 
     </header >
