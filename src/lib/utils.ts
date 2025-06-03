8409adc3a6e4f625563315920710a606d4350170
AbdulRehman-z <yousafbhaikhan10@gmail.com>
2025-06-03 20:09:13 +0500

    Add user utility functions and database imports

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,26 @@
+import { db, users } from "@/db";
 import { clsx, type ClassValue } from "clsx"
+import { eq } from "drizzle-orm";
 import { twMerge } from "tailwind-merge"
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+
+export function getInitials(name: string) {
+  return name.split(' ').map(word => word[0]).join('').toUpperCase().slice(0, 2);
+}
+
+
+export const getUser = async function (
+  email: string
+) {
+  try {
+    const user = await db.select().from(users).where(eq(users.email, email));
+    return user.at(0) ?? null;
+  } catch (error) {
+    console.error("Error fetching user by email:", error);
+    throw new Error("Failed to fetch user by email");
+  }
+};
