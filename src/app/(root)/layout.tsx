377cd8914a0651048fc710d23a724d25d49c47f1
AbdulRehman-z <yousafbhaikhan10@gmail.com>
2025-06-03 20:07:49 +0500

    Add authentication check to root layout

--- src/app/(root)/layout.tsx
+++ src/app/(root)/layout.tsx
@@ -1,11 +1,18 @@
+import { auth } from "@/auth";
 import Header from "@/components/custom/header";
+import { redirect } from "next/navigation";
 import { ReactNode } from "react";
 
-export default function Layout({ children }: { children: ReactNode }) {
+export default async function Layout({ children }: { children: ReactNode }) {
+  const session = await auth()
+  if (!session) {
+    redirect("/sign-in")
+  }
+
   return (
     <main className="root-container">
       <div className="max-w-7xl mx-auto">
-        <Header />
+        <Header session={session} />
         <div className="mt-20 pb-20">
           {children}
         </div>
