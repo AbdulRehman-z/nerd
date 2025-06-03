2a3a696dfd43c56a5a9eade2eab5b57657359efb
AbdulRehman-z <yousafbhaikhan10@gmail.com>
2025-06-03 20:08:09 +0500

    Refactor ImageKit authentication to use config object

--- src/app/api/upload-auth/route.ts
+++ src/app/api/upload-auth/route.ts
@@ -1,17 +1,16 @@
-import { env } from "@/lib/config"
+import { config } from "@/lib/config"
 import { getUploadAuthParams } from "@imagekit/next/server"
 
 export async function GET() {
   // Your application logic to authenticate the user
   // For example, you can check if the user is logged in or has the necessary permissions
   // If the user is not authenticated, you can return an error response
-
   const { token, expire, signature } = getUploadAuthParams({
-    privateKey: env.IMAGEKIT_PRIVATE_KEY as string, // Never expose this on client side
-    publicKey: env.NEXT_PUBLIC_IMAGEKIT_PUBLIC_KEY as string,
+    privateKey: config.env.imagekit.privateKey,
+    publicKey: config.env.imagekit.publicKey,
     // expire: 30 * 60, // Optional, controls the expiry time of the token in seconds, maximum 1 hour in the future
-    // token: "random-token", // Optional, a unique token for request
+    // token: "random-token", // Optional, a unique token for requestconfig.imagekit.publicKeyconfig.imagekit.publicKey
   })
 
-  return Response.json({ token, expire, signature, publicKey: env.NEXT_PUBLIC_IMAGEKIT_PUBLIC_KEY })
+  return Response.json({ token, expire, signature, publicKey: config.env.imagekit.publicKey })
 }
