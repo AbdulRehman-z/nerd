4b6e8faccf450748564c4e3d0e2404d08a91812c
AbdulRehman-z <yousafbhaikhan10@gmail.com>
2025-06-03 20:14:12 +0500

    Add database connection setup with Neon and Drizzle ORM

---
+++ src/db/index.ts
@@ -0,0 +1,49 @@
+import { config } from '@/lib/config';
+import { neon, neonConfig } from '@neondatabase/serverless';
+import { drizzle as DrizzleHttp } from 'drizzle-orm/neon-http';
+import * as usersSchema from "./schemas/user-schema"
+// @ts-ignore
+import ws from 'ws';
+
+let connectionString = config.env.databaseUrl;
+// Combine all schemas into a single object
+const schema = {
+  ...usersSchema,
+  // ...spread other schemas here
+};
+
+
+// Configuring Neon for local development
+if (config.env.nodeEnv === 'development') {
+  connectionString = 'postgres://postgres:postgres@db.localtest.me:5432/nerd';
+  neonConfig.fetchEndpoint = (host) => {
+    const [protocol, port] = host === 'db.localtest.me' ? ['http', 4444] : ['https', 443];
+    return `${protocol}://${host}:${port}/sql`;
+  };
+  const connectionStringUrl = new URL(connectionString);
+  neonConfig.useSecureWebSocket = connectionStringUrl.hostname !== 'db.localtest.me';
+  neonConfig.wsProxy = (host) => (host === 'db.localtest.me' ? `${host}:4444/v2` : `${host}/v2`);
+}
+neonConfig.webSocketConstructor = ws;
+
+const client = neon(connectionString as string);
+
+// Drizzle supports both HTTP and WebSocket clients. Choose the one that fits your needs:
+
+// HTTP Client:
+// - Best for serverless functions and Lambda environments
+// - Ideal for stateless operations and quick queries
+// - Lower overhead for single queries
+// - Better for applications with sporadic database access
+export const db = DrizzleHttp(client, { schema });
+
+
+// Export all schemas
+export { schema };
+// You can also export individual schemas if needed
+// export { auth, workflow };
+
+// Export specific tables/relations
+export const {
+  users
+} = schema;
