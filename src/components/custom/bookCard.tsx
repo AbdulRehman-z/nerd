227e62e07e153f69422433fe20a531c5ebe4a751
AbdulRehman-z <yousafbhaikhan10@gmail.com>
2025-06-12 15:08:51 +0500

    Fix link path in BookCard component to correctly route to book details

--- src/components/custom/bookCard.tsx
+++ src/components/custom/bookCard.tsx
@@ -7,7 +7,7 @@ import { Button } from "../ui/button";
 export default function BookCard({ id, title, genre, color, cover, isLoanedBook }: Book) {
   return (
     <li className={cn(isLoanedBook && "xs:w-52 w-full")}>
-      <Link href={`/books/${id}`} className={cn(isLoanedBook && "w-full flex flex-col items-center")} >
+      <Link href={`/book/${id}`} className={cn(isLoanedBook && "w-full flex flex-col items-center")} >
         <BookCover coverColor={color} coverImage={cover} />
         <div className={cn("mt-4", !isLoanedBook && "xs:max-w-40  max-w-28")}>
           <p className="book-title">{title}</p>
